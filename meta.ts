cb84300c5394098d7e42b2c2f0af148ca03c3e34
Elton M <eltonmarku@gmail.com>
2022-01-12 22:36:43 +0100

    fix meta

--- meta.ts
+++ meta.ts
@@ -4,7 +4,6 @@ export const meta = {
   colors: ["#FF4D91", "#000", "#ADACA6"],
   description:
     "Fabir resume template is a clean, classic, magazine style resume suitable for any job seeker. Simplistic, clean lines and columns make this a great choice for the professional looking to get noticed.",
-  name: "resumerise_theme_retro",
   platforms: ["DESKTOP", "INTERACTIVE", "MOBILE", "PRINT"],
   categories: [
     "EDUCATION",
