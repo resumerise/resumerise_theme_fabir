558bd02db5bc1e9e0811fd1352e7a82efac6d2d0
Khaan25 <ziarzai@hotmail.com>
2022-03-18 20:50:28 +0500

    added the files... CSS not working

--- meta.ts
+++ meta.ts
@@ -1,20 +1,12 @@
 import { ResumeriseMeta } from "./core-library.ts";
 
 export const meta = {
-  colors: ["#FF4D91", "#000", "#ADACA6"],
-  description:
-    "Fabir resume template is a clean, classic, magazine style resume suitable for any job seeker. Simplistic, clean lines and columns make this a great choice for the professional looking to get noticed.",
+  colors: ["#000", "#fff"],
+  description: "A starter template for resumerise.",
   platforms: ["DESKTOP", "INTERACTIVE", "MOBILE", "PRINT"],
-  categories: [
-    "EDUCATION",
-    "BUSINESS",
-    "FASHION",
-    "FOOD",
-    "SEO",
-    "HIGH_SCHOOL",
-  ],
-  styles: ["CLEAN", "BORDER", "ELEGANT", "SIMPLE"],
+  tags: ["starter", "blueprint", "retro"],
+  categories: ["EDUCATION"],
+  styles: ["CLEAN", "MINIMAL"],
   author: "resumerise",
   authorWebUrl: "http://resumerise.io",
-  subscription: "FREE"
 } as ResumeriseMeta;
